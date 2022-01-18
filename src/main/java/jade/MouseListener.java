07a021cffbd512ae6841accad1736554a0c6bae9
SloaneT <89420377+SloaneT@users.noreply.github.com>
2022-01-17 18:17:23 -0800

    updating mouse listener

--- src/main/java/jade/MouseListener.java
+++ src/main/java/jade/MouseListener.java
@@ -1,9 +1,103 @@
 package jade;
 
+// Many of these classes are singletons, which mean they only have
+// one object instantiated at a time
+
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+
 public class MouseListener {
     private static MouseListener instance;
     private double scrollX, scrollY;
     private double xPos, yPos, lastY, lastX;
-    private boolean mousebuttonPressed[] = new boolean[3];
+    private boolean mouseButtonPressed[] = new boolean[3];
     private boolean isDragging;
+
+    private MouseListener(){
+        // initializing these attributes helps avoid bugs
+        this.scrollX = 0.0;
+        this.scrollY = 0.0;
+        this.xPos = 0.0;
+        this.yPos = 0.0;
+        this.lastX = 0.0;
+        this.lastY = 0.0;
+    }
+
+    public static MouseListener get() {
+        if (MouseListener.instance == null) {
+            MouseListener.instance = new MouseListener();
+        }
+
+        return MouseListener.instance;
+    }
+
+    public static void mousePosCallback(long window, double xpos, double ypos){
+        get().lastX = get().xPos;
+        get().lastY = get().yPos;
+        get().xPos = xpos;
+        get().yPos = ypos;
+    }
+    // mods involve modifier keys e.g. "ctrl"
+    public static void mouseButtonCallback(long window, int button, int action, int mods){
+        if(action == GLFW_PRESS) {
+            // make sure user isn't pressing too many mouse buttons
+            if (button < get().mouseButtonPressed.length) {
+                get().mouseButtonPressed[button] = true;
+            }
+        }   else if (action == GLFW_RELEASE){
+                if (button < get().mouseButtonPressed.length) {
+                    get().mouseButtonPressed[button] = false;
+                    get().isDragging = false;
+            }
+        }
+    }
+    // callbacks need to be static for when they're passed to callback handler
+    // as opposed to variables, etc...
+    public static void mouseScrollCallback(long window, double xOffset, double yOffset){
+        get().scrollX = xOffset;
+        get().scrollY = yOffset;
+    }
+
+    public static void endFrame(){
+        get().scrollX = 0;
+        get().scrollY = 0;
+        get().lastX = get().xPos;
+        get().lastY = get().yPos;
+    }
+
+    public static float getX() {
+        return(float)get().xPos;
+    }
+
+    public static float getY() {
+        return(float)get().yPos;
+    }
+
+    public static float getDx() {
+        return(float)(get().lastX - get().xPos);
+    }
+
+    public static float getDy() {
+        return(float)(get().lastY - get().yPos);
+    }
+
+    public static float getScrollX() {
+        return (float)get().scrollX;
+    }
+
+    public static float getScrollY() {
+        return (float)get().scrollY;
+    }
+
+    public static boolean isDragging() {
+        return get().isDragging;
+    }
+
+    public static boolean mouseButtonDown(int button) {
+        if (button < get().mouseuttonPressed.length) {
+            return get().mouseButtonPressed[button];
+        } else {
+            return false;
+        }
+    }
 }
